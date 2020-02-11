6e25ea9794e09199c6d389a4a3f8653ef9aa6ff2
SuumCuique <Vlad.V.Kuznetsov@hotmail.com>
2020-02-11 15:42:41 +0200

    final

--- Tapper.py
+++ Tapper.py
@@ -4,11 +4,9 @@ img = Image.open(argv[1])
 pixels = list(img.getdata())
 width, height = img.size
 pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-counter,string,mas=0,0,{}
+counter,result=0,0
 for x in range(0,106):
-    mas[x] = {}
     for y in range(0,17):
-        mas[x][y] = 1 if pixels[y][105-x][1]==0 else 0
-        string += mas[x][y]<<counter
+        result += (1 if pixels[y][105-x][1]==0 else 0)<<counter
         counter+=1
-print(string*17)
+print(result*17)
